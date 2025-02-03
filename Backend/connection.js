245665406e04330def9b2ee3f70449336c012b86
RahmaJumana <rahmajumanap@gmail.com>
2025-02-03 11:57:01 +0530

    Update connection.js

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose
   .connect(
-   " "
+   " mongodb+srv://rjp:rahma@cluster0.mfydx.mongodb.net/blogapp?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log("Connected to DB");
